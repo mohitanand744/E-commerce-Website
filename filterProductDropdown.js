73932863fd7c936056270cb54e1d20a8e7c92634
Mohit Jha <95844984+mohitanand744@users.noreply.github.com>
2024-05-28 19:28:19 +0530

    Added      functionality to filter products

--- filterProductDropdown.js
+++ filterProductDropdown.js
@@ -1,13 +1,34 @@
 export let filterProductDropdown = () => {
   let activeFilterbtn = document.querySelector(".activeFilterbtn");
+  let deactivateFilterbtn = document.querySelector(".deactivateFilterbtn");
+  let categoryContainer = document.querySelector(".categoryContainer");
+  let Category = document.querySelectorAll(".Category");
 
-  window.addEventListener("scroll",()  =>  {
-    
-    if (window.scrollY >= 200) {
+  window.addEventListener("scroll", () => {
+    if (window.scrollY >= 240) {
       activeFilterbtn.style.display = "flex";
     } else {
       activeFilterbtn.style.display = "none";
+      categoryContainer.style.display = "none";
+      deactivateFilterbtn.style.display = "none";
     }
   });
-  
+
+  activeFilterbtn.addEventListener("click", () => {
+    categoryContainer.style.width = "10rem";
+    Category.forEach((Category) => {
+      Category.style.display = "flex";
+    });
+    activeFilterbtn.style.display = "none";
+    deactivateFilterbtn.style.display = "flex";
+  });
+
+  deactivateFilterbtn.addEventListener("click", () => {
+    categoryContainer.style.width = "0rem";
+    Category.forEach((Category) => {
+      Category.style.display = "none";
+    });
+    activeFilterbtn.style.display = "flex";
+    deactivateFilterbtn.style.display = "none";
+  });
 };
