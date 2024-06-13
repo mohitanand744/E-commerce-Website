3e95139c71b00c063d03f4638f6b03ab85733118
Mohit Jha <95844984+mohitanand744@users.noreply.github.com>
2024-06-13 20:09:46 +0530

    searchFunctionality Solution Using Regular Expression.

--- searchFunctionality.js
+++ searchFunctionality.js
@@ -1,3 +1,8 @@
+import { displaySearchProd } from "./displaySearchProd";
+
+
+// ! In this function if user will enter men Then it's returning men's product as well as womens product because it's targeting the last three litres of women.
+
 /* export let searchFunctionality = (items) => {
   let userInputs = document.querySelectorAll(".userInput");
 
@@ -14,7 +19,8 @@
   });
 }; */
 
-import { displaySearchProd } from "./displaySearchProd";
+
+// ! In this function user have to enter Exact word to get related products but also this is only matching the first word of user input. ex: If User will inter men Shoes than this function will target only first word "men".
 
 /* export let searchFunctionality = (items) => {
   let userInputs = document.querySelectorAll(".userInput");
@@ -34,7 +40,9 @@ import { displaySearchProd } from "./displaySearchProd";
   });
 }; */
 
-export let searchFunctionality = (items) => {
+// ! In This Code The User Have To Inter Exact word to get related products.
+
+/* export let searchFunctionality = (items) => {
   let userInputs = document.querySelectorAll(".userInput");
 
   userInputs.forEach((input) => {
@@ -56,5 +64,29 @@ export let searchFunctionality = (items) => {
       displaySearchProd(filterProd, inputValue);
     });
 
+  });
+};
+ */
+
+
+
+
+// * Solution Using Regular Expression.
+
+export let searchFunctionality = (items) => {
+  let userInputs = document.querySelectorAll(".userInput");
+
+  userInputs.forEach((input) => {
+    input.addEventListener("keyup", () => {
+      let inputValue = input.value.toLowerCase().trim();
+
+      let regex = new RegExp(`\\b${inputValue}|^${inputValue}, 'i'`);
+
+      let filterProd = items.filter((prods) => {
+        return regex.test(prods.category.toLowerCase());
+      });
+
+      displaySearchProd(filterProd, inputValue);
+    });
   });
 };
